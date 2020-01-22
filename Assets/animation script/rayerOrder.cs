620548c22ce67b6068820113eba70b8803f4e379
gann <daiki_17n1100521@nnn.ed.jp>
2020-01-23 00:17:11 +0900

    fix

---
+++ Assets/animation script/rayerOrder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rayerOrder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var render = GetComponent<MeshRenderer>();
+        if (render != null)
+        {
+            render.sortingOrder = 6;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
